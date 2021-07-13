0279d9a66758a667cea1b3d2545e953c3f8c833e
Suhani5678 <56391109+Suhani5678@users.noreply.github.com>
2021-07-13 10:04:52 +0530

    Create program-1.cpp

---
+++ program-1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namspace std;
+
+int main(){
+    char op;
+    float a, b ;
+    
+    cout << "Enter operator: +, -, *, /:";
+    cin >> op;
+    
+    cout << "Enter two operands: ";
+    cin >> a >> b;
+    
+    switch(op) {
+        case '+':
+            cout << a << "+" << b << "=" << a + b ;
+            break;
+            
+        case '-':
+            cout << a << "-" << b << "=" << a - b ;
+            break;
+         
+        case '*':
+            cout << a << "*" << b << "=" << a * b ;
+            break;
+            
+        case '/':
+            cout << a << "/" << b << "=" << a / b ;
+            break;
+            
+        default:
+            cout << "operator is not correct";
+            break;
+            
+         }
+         
+         return 0;
+         
+     }
